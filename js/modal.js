9e3d927b9fb018ceced02ba1bc74acc55fd649c0
Alona1m <sergei.ua.kh@gmail.com>
2022-08-21 15:11:46 +0300

    Выполняет адаптив под мобильное устройство

--- js/modal.js
+++ js/modal.js
@@ -1,20 +1,4 @@
 (() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
-  };
-
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
-
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
-  }
-})();
-
-
-(() => {
   const menuBtnRf = document.querySelector("[data-menu-button]");
   const mobileMemuRef = document.querySelector("[data-menu]");
   
@@ -25,4 +9,19 @@
     mobileMemuRef.classList.toggle("is-open");    
   });
 
+})();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
 })();
